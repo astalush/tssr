0d68e4e91295c8fd27236cac71369477f2532eb3
astalush <astalush@yahoo.com>
2020-08-14 20:44:36 +0200

    Update installglpi.sh

--- installglpi.sh
+++ installglpi.sh
@@ -45,8 +45,9 @@ echo "                                                                          
 echo "                                                                                             ";
 }
 
+
 #fonction pour double verification du MDP root MYSQL
-password_check () {
+password_check_mysql () {
 checkmdp=$DATABASE_PASS
 while true; do
     read -s -p "Entrez le nouveau MDP: " checkmdp
@@ -60,7 +61,7 @@ DATABASE_PASS=$checkmdp
 }
 
 #fonction pour double verification mdp utilisateur glpi
-userpassword_check () {
+userpassword_check_glpi () {
 check=$userpass
 while true; do
     read -s -p "Entrez le nouveau MDP: " check
@@ -73,22 +74,9 @@ done
 userpass=$check
 }
 
-#fonction pour continuer apres qu'une touche soit pressée et pour effacer le fichier install.php de GLPI
-presskey_check () {
-echo ""
-while [ true ] ; do
-read -t 1200 -n 1
-if [ $? = 0 ] ; then
-rm /var/www/glpi/install/install.php && sleep 1 && tssr2020
-exit;
-fi
-done
-}
-
-
 ###MISE A JOUR DES DEPOTS
 #on va effacer l'ancien fichier sources.list
-echo -e "$Red \n MISE A JOUR DES DEPOTS DEBIAN $Color_Off"
+echo -e "$Red \nMISE A JOUR DES DEPOTS DEBIAN...$Color_Off"
 rm /etc/apt/sources.list
 sleep 3
 
@@ -101,7 +89,6 @@ echo "deb-src http://deb.debian.org/debian-security/ buster/updates main contrib
 echo "" >> /etc/apt/sources.list
 echo "deb http://deb.debian.org/debian buster-updates main contrib non-free" >> /etc/apt/sources.list
 echo "deb-src http://deb.debian.org/debian buster-updates main contrib non-free" >> /etc/apt/sources.list
-echo "Mise a jour suite a la commande apt update"
 sleep 3
 #faire une mise a jour avec apt update
 sudo apt update
@@ -110,36 +97,36 @@ sudo apt update
 sudo apt upgrade -y
 
 #installation apache
-echo -e "$Cyan \n Installation apache2 $Color_Off"
+echo -e "$Cyan \nInstallation apache2 $Color_Off"
 sleep 3
 sudo apt install apache2 apache2-doc apache2-utils libexpat1 ssl-cert -y
 
 #installation PHP
-echo -e "$Yellow \n Installation PHP $Color_Off"
+echo -e "$Green \nInstallation PHP $Color_Off"
 sleep 3
 apt install curl net-tools php7.3 php7.3-curl php7.3-zip php7.3-gd php7.3-intl php-pear php-imagick php7.3-imap php-memcache php7.3-pspell php7.3-recode php7.3-tidy php7.3-xmlrpc php7.3-xsl php7.3-mbstring php-gettext php7.3-ldap php-cas php-apcu libapache2-mod-php7.3 php7.3-mysql libbz2-dev php7.3-bz2 -y
 
 #restart apache2
-echo -e "$Red \n Redemarrage apache2 $Color_Off"
+echo -e "$Cyan \nRedemarrage apache2 $Color_Off"
 sleep 3
 sudo systemctl restart apache2 &> /dev/null
 
 #installation mariaBD
-echo -e "$Green \n Installation MariaDB $Color_Off"
+echo -e "$Green \nInstallation MariaDB $Color_Off"
 apt-get install mariadb-server mariadb-client -y
 sleep 3
 
 #parametrage du MDP root pour MYSQL
 echo "---------------------------------------------------------------------------------------------------"
-echo -e "$Cyan \n| MYSQL n'a pas de MDP defini. Merci d'entrer un MDP pour l'utilisateur root dans MYSQL |${Color_Off}"
+echo -e "$Red \n| MYSQL n'a pas de MDP defini. Merci d'entrer un MDP pour l'utilisateur root dans MYSQL |${Color_Off}"
 echo "---------------------------------------------------------------------------------------------------"
 echo ""
-password_check
+password_check_mysql
 echo ""
 mysqladmin -u root password $DATABASE_PASS
 
 # mysql_secure_installation.sql
-echo -e "$Red \n Sécurisation du serveur MySQL ${Color_Off}"
+echo -e "$Red \nSécurisation du serveur MySQL ${Color_Off}"
 sleep 3
 mysql -uroot -p${DATABASE_PASS} -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 mysql -uroot -p${DATABASE_PASS} -e "DELETE FROM mysql.user WHERE User='';"
@@ -148,50 +135,48 @@ mysql -uroot -p${DATABASE_PASS} -e "FLUSH PRIVILEGES;"
 
 #creation base de donnees GLPI
 echo "---------------------------------------------------------------------------------------------------"
-echo -n -e "$Red \n Entrez le nom de la base de donnees que vous voulez creer pour GLPI: ${Color_Off}";read dbname
+echo -n -e "$Red \nEntrez le nom de la base de donnees que vous voulez creer pour GLPI: ${Color_Off}";read dbname
 echo "---------------------------------------------------------------------------------------------------"
 echo ""
-echo -e "$Red \n Creation de la nouvelle base de donnees ${dbname} dans MySQL... ${Color_Off}"
+echo -e "$Red \nCreation de la nouvelle base de donnees ${Color_Off}${dbname}$Red dans MySQL... ${Color_Off}"
 mysql -uroot -p${DATABASE_PASS} -e "CREATE DATABASE ${dbname} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-echo -e "$Red \n Creation OK!${Color_Off}"
-echo ""
-echo -e "$Red \n Listez les base de donnees existantes... ${Color_Off}"
+echo -e "$Red \nCreation OK!${Color_Off}"
+echo -e "$Red \nListez les base de donnees existantes... ${Color_Off}"
 mysql -uroot -p${DATABASE_PASS} -e "show databases;"
+sleep 3
+echo -n -e "$Red \nEntrez le nom de l'utilisateur/administrateur pour la nouvelle base de donnees ${Color_Off}${dbname}$Red : ${Color_Off}";read username
+echo -e "$Red \nEntrez le mot de passe pour l'utilisateur/administrateur ${Color_Off}${username} $Red(Info : le MDP va etre caché quand vous le tapez): ${Color_Off}"
 echo ""
-sleep 2
-echo -n -e "$Red \n Entrez le nom de l'utilisateur/administrateur pour la nouvelle base de donnees ${dbname}: ${Color_Off}";read username
-echo -e "$Red \n Entrez le mot de passe pour l'utilisateur/administrateur ${username} (Info : le MDP va etre caché quand vous le tapez): ${Color_Off}"
+userpassword_check_glpi
 echo ""
-userpassword_check
-echo ""
-echo ""
-echo -e "$Red \n Creation du nouvel l'utilisateur/administrateur ${username} dans MYSQL... ${Color_Off}"
+echo -e "$Red \nCreation du nouvel l'utilisateur/administrateur ${Color_Off}${username} $Red dans MYSQL... ${Color_Off}"
 mysql -uroot -p${DATABASE_PASS} -e "CREATE USER ${username}@localhost IDENTIFIED BY '${userpass}';"
-echo -e "$Red \n Creation OK! ${Color_Off}"
-echo ""
-echo -e "$Red \n Granting ALL privileges on ${dbname} to ${username}! ${Color_Off}"
+echo -e "$Red \nCreation OK! ${Color_Off}"
+echo -e "$Red \nGranting ALL privileges on ${Color_Off}${dbname} to ${username}! "
 mysql -uroot -p${DATABASE_PASS} -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${username}'@'localhost';"
 mysql -uroot -p${DATABASE_PASS} -e "FLUSH PRIVILEGES;"
 
 #telecharger la derniere version du glpi depuis github
-echo -e "$Cyan \n Telechargement de la dernier version de GLPI $Color_Off"
+echo -e "$Cyan \nTelechargement de la dernier version de GLPI... $Color_Off"
 cd /tmp
 wget https:$(wget https://github.com/glpi-project/glpi/releases/latest.tgz -O - | egrep '/.*/.*/.*tgz' -o)
 sleep 3
 
 #dezarchiver glpi
-echo -e "$Cyan \n Desarchiver GLPI $Color_Off"
+echo -e "$Cyan \nDesarchiver GLPI $Color_Off"
 tar -xzvf glpi-*.tgz &> /dev/null
 sleep 3
 
 #deplacer glpi dans /var/www
-echo -e "$Green \n Deplacer GLPI dans le dossier /var/www $Color_Off"
+echo -e "$Cyan \nDeplacer GLPI dans le dossier /var/www ...$Color_Off"
+sleep 2
 cp -r glpi /var/www
-echo -e "$Green \n Changement des droits d'access sur le dossier /var/www/glpi pour www-data $Color_Off"
+echo -e "$Cyan \nChangement des droits d'access sur le dossier /var/www/glpi pour www-data ...$Color_Off"
 chown -R www-data /var/www/glpi
+sleep 2
 
 #parametrage glpi.conf
-echo -e "$Red \n Creation et modification du fichier conf de glpi.conf pour apache2.. ${Color_Off}"
+echo -e "$Cyan \nCreation et modification du fichier conf de glpi.conf pour apache2... ${Color_Off}"
 if [ -e /etc/apache2/sites-available/000-default.conf ];
 then rm /etc/apache2/sites-available/000-default.conf
 fi
@@ -216,39 +201,63 @@ echo "</VirtualHost>" >> /etc/apache2/sites-available/glpi.conf
 sleep 3
 
 #activation du hote GLPI dans apache2
-echo -e "$Cyan \n Activation du hote glpi dans apache2.. ${Color_Off}"
+echo -e "$Cyan \nActivation du hote glpi dans apache2... ${Color_Off}"
 sudo ln -s /etc/apache2/sites-available/glpi.conf /etc/apache2/sites-enabled/glpi.conf
 sleep 3
-echo -e "$Cyan \n Activation des modules apache2... ${Color_Off}"
+echo -e "$Cyan \nActivation des modules apache2... ${Color_Off}"
 sudo a2enmod rewrite &> /dev/null
 sleep 3
-echo -e "$Cyan \n Redemarrage apache2... ${Color_Off}"
+echo -e "$Cyan \nRedemarrage apache2... ${Color_Off}"
 sudo systemctl restart apache2 &> /dev/null
 sleep 3
 chgrp www-data /var/www/glpi/{config,files,files/_{dumps,sessions,cron,graphs,lock,plugins,tmp,rss,uploads,pictures,log}}
-echo "---------------------------------------------------------------------------------------------------"
-echo -e "$Green \n SUCCESS! Votre MDP root pour MySQL password est: ${DATABASE_PASS} ${Color_Off}"
 
 #chercher l'addresse IP du serveur GLPI
 IPAD=`ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p'`
 
-#affichage des infos pour le serveur GLPI
-echo ""
-echo "---------------------------------------------------------------------------------------------------"
-echo -e "$Red \n L'adresse IP du GLPI: http://$IPAD/ ${Color_Off}"
-echo "---------------------------------------------------------------------------------------------------"
-echo -e "$Red \n Pour vous connecter pour la premiere fois sur la base de donnees GLPI merci d'utiliser: ${Color_Off}"
-echo "---------------------------------------------------------------------------------------------------"
-echo -e "$Yellow \n| Adresse IP du GLPI : http://$IPAD/ ${Color_Off}|"
-echo -e "$Yellow \n| Serveur SQL (Maria DB ou MYSQL) : localhost ${Color_Off}|"
-echo -e "$Yellow \n| Utilisateur SQL: ${username} ${Color_Off}|"
-echo -e "$Yellow \n| Mot de passe SQL: ${userpass} ${Color_Off}|"
-echo "---------------------------------------------------------------------------------------------------"
-echo ""
-echo -e "$Red \n Merci d'attendre 15 seconds SVP... ${Color_Off}"
+#timestamps GLPI pour MySQL
+mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -uroot -p${DATABASE_PASS} mysql &> /dev/null
+mysql -uroot -p${DATABASE_PASS} -e "GRANT SELECT ON mysql.time_zone_name TO '${username}'@'localhost';"
+php /var/www/glpi/bin/console glpi:migration:timestamps --no-interaction &> /dev/null
 
-sleep 15
-#affichage des infos supplementaires et appelle de la fonction presskey
-echo -e "$Red \n Pour des raisons de securité le fichier install.php de GLPI sera effacé a la fin de ce script. ${Color_Off}"
-echo -e "$Red \n Merci de bien parametrer GLPI jusqu'a la FIN avant de valider en appuyant sur n'importe quelle touche .. ${Color_Off}"
-presskey_check
+#restart MYSQL / MariaDB
+echo -e "$Green \nRestart MYSQL / MariaDB et apache2...$Color_Off"
+sudo systemctl restart apache2 &> /dev/null
+/etc/init.d/mysql restart
+
+#parametrage en CLI pour GLPI
+php /var/www/glpi/bin/console db:install -r -f -Lfr_FR -Hlocalhost -P3306 -d$dbname -u$username -p$userpass --no-interaction
+php /var/www/glpi/bin/console db:configure -r -Hlocalhost -P3306 -d$dbname -u$username -p$userpass --no-interaction
+sleep 5
+echo -e "$Cyan \nRedemarrage apache2... ${Color_Off}"
+
+sudo chown -R www-data:www-data /var/www/glpi &> /dev/null
+sudo systemctl restart apache2 &> /dev/null
+sleep 5
+
+#effacer install.php pour des raisons de securite
+echo -e "$Cyan \nEffacer le fichier install du GLPI (/install/install.php) pour des raisons de securite...${Color_Off}"
+rm -rf /var/www/glpi/install/install.php
+echo ""
+
+#affichage des infos pour le serveur GLPI
+echo "---------------------------------------------------------------------------------------------------"
+php /var/www/glpi/bin/console glpi:system:check_requirements
+echo ""
+echo "---------------------------------------------------------------------------------------------------"
+echo -e "$Red \nSUCCESS! Votre MDP root pour MySQL est:${Color_Off} ${DATABASE_PASS} "
+echo "---------------------------------------------------------------------------------------------------"
+echo ""
+echo -e "$Cyan \n Infos sur le nouveau GLPI: ${Color_Off}"
+echo "---------------------------------------------------------------------------------------------------"
+echo -e "$Cyan \n| Adresse IP du GLPI :${Color_Off} http://$IPAD/ |"
+echo -e "$Cyan \n| Serveur SQL (Maria DB ou MYSQL) :${Color_Off} localhost ${Color_Off}|"
+echo -e "$Cyan \n| Base de donnees MYSQL pour GLPI :${Color_Off} ${dbname} ${Color_Off}|"
+echo -e "$Cyan \n| Utilisateur SQL:${Color_Off} ${username} |"
+echo -e "$Cyan \n| Mot de passe SQL:${Color_Off} ${userpass} ${Color_Off}|"
+echo "---------------------------------------------------------------------------------------------------"
+echo ""
+sleep 3
+
+#appel de la fonctions tssr2020 affichage
+tssr2020
